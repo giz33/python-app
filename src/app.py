cab537fc320a06864b640aa3c07a5876e1db2c8f
LucasRizzi <lucas.rizzi@hotmail.com>
2025-06-04 15:32:11 -0300

    Add CI Workflow

--- src/app.py
+++ src/app.py
@@ -9,7 +9,8 @@ def details():
     return jsonify(
         {
             'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
-            'hostname': socket.gethostname()
+            'hostname': socket.gethostname(),
+            'message': 'You are doing great, human!'
         }
     )
 
